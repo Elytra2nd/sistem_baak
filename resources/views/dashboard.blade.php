bb4c77638c7ef2f1fda0f3f6f84b9fa574b7742b
M. Imam Firdaus <mimamfirdaus5@gmail.com>
2025-04-07 23:46:05 +0700

    perbaiki kode

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,17 @@
 {{-- resources/views/dashboard.blade.php --}}
-<x-app-layout :title="'Dashboard'" :header="'Dashboard'">
+<x-app-layout>
     <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-md rounded-md p-6">
-                <x-welcome />
+                {{-- <x-welcome /> --}}
+
+                {{-- Statistik Jumlah Mahasiswa --}}
+                <div class="mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                    <div class="bg-blue-100 p-4 rounded-lg shadow text-center">
+                        <div class="text-sm font-medium text-blue-800">Jumlah Mahasiswa</div>
+                        <div class="text-3xl font-bold text-blue-900">{{ $jumlahMahasiswa }}</div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,13 +13,9 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
-
 
 Route::middleware(['auth'])->group(function () {
     Route::resource('mahasiswa', MahasiswaController::class)->names('mahasiswa');
 });
-
